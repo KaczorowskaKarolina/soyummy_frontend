59206cca6c943ff221531c403e2cc619c2054da1
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2024-02-29 23:56:52 +0100

    Create basic template for SharedLayout component

---
+++ src/components/Templates/SharedLayout/SharedLayout.jsx
@@ -0,0 +1,11 @@
+import css from './SharedLayout.module.css';
+
+import { Header } from 'components/Organisms/Header/Header.jsx';
+import { Footer } from 'components/Organisms/Footer/Footer';
+import { Navigation } from 'components/Organisms/Navigation/Navigation';
+
+import { Outlet } from 'react-router-dom';
+
+const SharedLayout = () => {};
+
+export { SharedLayout };
