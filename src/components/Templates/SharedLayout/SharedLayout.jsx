82c68656e1be9b21f6e11aaf2a293f833475f01e
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2024-03-01 23:41:19 +0100

    Add functionality for shared layout rendering

--- src/components/Templates/SharedLayout/SharedLayout.jsx
+++ src/components/Templates/SharedLayout/SharedLayout.jsx
@@ -1,11 +1,28 @@
 import css from './SharedLayout.module.css';
 
 import { Header } from 'components/Organisms/Header/Header.jsx';
-import { Footer } from 'components/Organisms/Footer/Footer';
-import { Navigation } from 'components/Organisms/Navigation/Navigation';
+import { Footer } from 'components/Organisms/Footer/Footer.jsx';
+import { Navigation } from 'components/Organisms/Navigation/Navigation.jsx';
 
 import { Outlet } from 'react-router-dom';
+import { useState } from 'react';
 
-const SharedLayout = () => {};
+const SharedLayout = () => {
+  const [menuState, setMenuState] = useState(false);
+
+  if (menuState) {
+    return <Navigation closeMenu={setMenuState} />;
+  }
+  return (
+    <div className={css.container}>
+      <Header openMenu={setMenuState} />
+      <div className={css.titleBox}>
+        <h2 className={css.title}>Title</h2>
+      </div>
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
 
 export { SharedLayout };
