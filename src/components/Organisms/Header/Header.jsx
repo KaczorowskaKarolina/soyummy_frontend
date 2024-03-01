70adf809f9fee2b3d3dfb46c41e348ece7eb851a
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2024-03-01 23:43:52 +0100

    Add functionality for header rendering

--- src/components/Organisms/Header/Header.jsx
+++ src/components/Organisms/Header/Header.jsx
@@ -1,5 +1,40 @@
 import css from './Header.module.css';
 
-const Header = () => {};
+import { ReactComponent as HeaderLogo } from '../../../images/icons/logo-header-icon.svg';
+import { ReactComponent as BurgerMenu } from '../../../images/icons/burger-icon.svg';
+import { ReactComponent as Switch } from '../../../images/icons/switch-light-icon.svg';
+
+import { Navbar } from 'components/Molecules/Navbar/Navbar.jsx';
+
+import { Link } from 'react-router-dom';
+import { UserLogo } from 'components/Molecules/UserLogo/UserLogo.jsx';
+
+const Header = ({ openMenu }) => {
+  let width = window.innerWidth;
+
+  return (
+    <div className={css.container}>
+      <div className={css.box}>
+        <Link to="/" className={css.logo}>
+          <HeaderLogo />
+        </Link>
+        {width < 1240 ? null : <Navbar />}
+        <div className={css.rightContainer}>
+          <div className={css.userMenu}>
+            <UserLogo />
+            {/* <div className={css.avatar}>
+              <ProfilePhoto />
+            </div>
+            <p className={css.name}>Name</p> */}
+          </div>
+          <BurgerMenu openMenu={openMenu} />
+          <div className={css.switch}>
+            <Switch />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export { Header };
