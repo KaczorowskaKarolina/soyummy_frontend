557e051cbaf754c7a84275eb036bda7c922627ef
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2024-03-01 23:43:05 +0100

    Add functionality for navbar rendering

--- src/components/Molecules/Navbar/Navbar.jsx
+++ src/components/Molecules/Navbar/Navbar.jsx
@@ -1,5 +1,24 @@
 import css from './Navbar.module.css';
+import links from './links.json';
 
-const Navbar = () => {};
+import { NavigationLink } from 'components/Atoms/NavigationLink/NavigationLink.jsx';
+
+const Navbar = () => {
+  return (
+    <nav className={css.navigation}>
+      <ul className={css.list}>
+        {links.map((link, index) => (
+          <li key={index} className={css.item}>
+            <NavigationLink
+              path={link.path}
+              title={link.title}
+              icon={link.icon}
+            />
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
 export { Navbar };
