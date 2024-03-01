d5b6e26ec872f6ac1c19dd152337cde30d33ad95
Dag <dagmara.szadkowska@wp.pl>
2024-03-01 11:59:06 +0100

    Fixed form styles and SignInForm

--- src/pages/signIn/components/Organisms/SignInForm.jsx
+++ src/pages/signIn/components/Organisms/SignInForm.jsx
@@ -1,20 +1,24 @@
 import { RectangleButton } from '../../../../components/Atoms/RectangleButton/RectangleButton.jsx';
 import { SignInInputs } from '../Molecules/SignInInputs.jsx';
-import { SignInLink } from '../Atoms/SignInLink.jsx'
+import { SignInLink } from '../Atoms/SignInLink.jsx';
 import css from './SignInForm.module.css';
 
 const SignInForm = () => {
   return (
-    <div className={css.SignInForm}>
+    <form
+      // action=""
+      method="post" 
+      className={css.SignInForm}
+      >
       <div className={css.SignInModal}>
         <h2 className={css.SignInTitle}>Sign In</h2>
         <SignInInputs />
         <div className={css.SignInButton}>
-          <RectangleButton title="Sign In" size="large" />
+          <RectangleButton title="Sign In" size="large" type="submit"/>
         </div>
       </div>
       <SignInLink />
-    </div>
+    </form>
   );
 };
 
