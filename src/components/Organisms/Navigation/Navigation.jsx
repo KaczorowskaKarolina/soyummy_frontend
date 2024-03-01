f1dcb0e008b7947d349f7d80fa47c67fe05385d5
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2024-03-01 23:42:05 +0100

    Add functionality for navigation rendering

--- src/components/Organisms/Navigation/Navigation.jsx
+++ src/components/Organisms/Navigation/Navigation.jsx
@@ -1,11 +1,41 @@
 import css from './Navigation.module.css';
 
-import { Navbar } from 'components/Molecules/Navbar/Navbar';
+import { ReactComponent as LogoIcon } from '../../../images/icons/logo-header-icon.svg';
+import { ReactComponent as CloseIcon } from '../../../images/icons/close-icon.svg';
+import { ReactComponent as Switch } from '../../../images/icons/switch-light-icon.svg';
+
+import { Navbar } from 'components/Molecules/Navbar/Navbar.jsx';
 
 import { Link } from 'react-router-dom';
 
-/* import svg icons here - logo and close button*/
-
-const Navigation = () => {};
+const Navigation = ({ closeMenu }) => {
+  return (
+    <div className={css.container}>
+      <div className={css.icons}>
+        <Link
+          to="/"
+          onClick={() => {
+            closeMenu(false);
+          }}
+          className={css.logo}
+        >
+          <LogoIcon />
+        </Link>
+        <button
+          onClick={() => {
+            closeMenu(false);
+          }}
+          className={css.closeButton}
+        >
+          <CloseIcon />
+        </button>
+      </div>
+      <Navbar />
+      <div className={css.switch}>
+        <Switch />
+      </div>
+    </div>
+  );
+};
 
 export { Navigation };
