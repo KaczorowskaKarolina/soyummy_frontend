d71c5ad158bd202fde3bdd0beac76c21a7023d0f
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-03-02 14:14:34 +0100

    Add functionality to button "Other categories"

--- src/components/client/pages/home/Templates/HomeTemplate.jsx
+++ src/components/client/pages/home/Templates/HomeTemplate.jsx
@@ -1,14 +1,15 @@
 // import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 import { HomeOrganism } from '../Organisms/HomeOrganism';
-// import { changeQuery } from 'client/redux/categories/operations';
 import { HomeAtomButtonLoadMore } from '../Atoms/HomeAtomButtonLoadMore';
 
 import css from './HomeTemplate.module.css';
 
 const HomeTemplate = ({ recipesByCategory }) => {
+  const navigate = useNavigate();
   const handleLoadMore = () => {
-    console.log('Load more button');
+    navigate('/categories/Breakfast', { replace: true });
   };
   return (
     <div className={css.HomeTemplate}>
