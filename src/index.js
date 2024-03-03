f3e48a52046e8e8c96a55dcc31e777dc96e35931
mattmalicki <mateusz.malicki96@gmail.com>
2024-03-03 09:45:42 +0100

    Updating path of imported selectors.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import { App } from 'components/App';
 import { Provider } from 'react-redux';
-import { store, persistor } from 'redux/store';
+import { store, persistor } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 
