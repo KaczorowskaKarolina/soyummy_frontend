070877318d7d47c826182f8c7eec4b631e9d974d
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2024-03-04 00:47:25 +0100

    Add route for SharedLayout component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-
+import { SharedLayout } from 'Templates/SharedLayout/SharedLayout.jsx';
 import { StartPage } from '../pages/startPage/StartPage.jsx';
 import { SignIn } from '../pages/signIn/SignIn.jsx';
 import { Registration } from '../pages/registration/Registration.jsx';
@@ -19,20 +19,20 @@ const App = () => {
         <Route path="/soyummy_frontend" element={<StartPage />} />
         <Route path="/registration" element={<Registration />} />
         <Route path="/signin" element={<SignIn />} />
-        {/* <Route path="/" element={<SharedLayout />}> */}
-        {/* <Route index element={<MainPage />} /> */}
-        {/* <Route
+        <Route path="/" element={<SharedLayout />}>
+          {/* <Route index element={<MainPage />} /> */}
+          {/* <Route
             path="/categories/:categoryName"
             element={<CategoriesPage />}
           /> */}
-        {/* <Route path="/add" element={<AddRecipesPage />} /> */}
-        {/* <Route path="/my" element={<MyRecipesPage />} /> */}
-        {/* <Route path="/favorite" element={<FavoritePage />} /> */}
-        {/* <Route path="/shopping-list" element={<ShoppingListPage />} /> */}
-        {/* <Route path="/search" element={<SearchPage />} /> */}
-        {/* <Route path="/recipe/:recipeId" element={<RecipePage />} /> */}
-        {/* <Route path="*" element={<Page404 />} /> */}
-        {/* </Route> */}
+          {/* <Route path="/add" element={<AddRecipesPage />} /> */}
+          {/* <Route path="/my" element={<MyRecipesPage />} /> */}
+          {/* <Route path="/favorite" element={<FavoritePage />} /> */}
+          {/* <Route path="/shopping-list" element={<ShoppingListPage />} /> */}
+          {/* <Route path="/search" element={<SearchPage />} /> */}
+          {/* <Route path="/recipe/:recipeId" element={<RecipePage />} /> */}
+          {/* <Route path="*" element={<Page404 />} /> */}
+        </Route>
       </Routes>
     </div>
   );
