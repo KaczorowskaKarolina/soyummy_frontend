ca9501980181c8ece686cb9856d72828db1f3500
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2024-03-03 23:48:47 +0100

    Create component for rendering social media links

---
+++ src/components/Molecules/FooterIcons/FooterIcons.jsx
@@ -0,0 +1,16 @@
+import css from './FooterIcons.module.css';
+
+import { FooterIcon } from 'components/Atoms/FooterIcon/FooterIcon.jsx';
+
+const FooterIcons = () => {
+  return (
+    <div className={css.footerIcons}>
+      <FooterIcon socialMedia="facebook" />
+      <FooterIcon socialMedia="youtube" />
+      <FooterIcon socialMedia="twitter" />
+      <FooterIcon socialMedia="instagram" />
+    </div>
+  );
+};
+
+export { FooterIcons };
