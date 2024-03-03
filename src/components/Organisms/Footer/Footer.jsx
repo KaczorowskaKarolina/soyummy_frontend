c973f84fa9b375867d955c0d8335286927c0b679
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2024-03-03 23:57:40 +0100

    Add functionality for rendering Footer component

--- src/components/Organisms/Footer/Footer.jsx
+++ src/components/Organisms/Footer/Footer.jsx
@@ -1,5 +1,25 @@
 import css from './Footer.module.css';
 
-const Footer = () => {};
+import { FooterIcons } from 'components/Molecules/FooterIcons/FooterIcons.jsx';
+import { FooterInfo } from 'components/Molecules/FooterInfo/FooterInfo.jsx';
+import { FooterLinks } from 'components/Molecules/FooterLinks/FooterLinks.jsx';
+import { Newsletter } from 'components/Molecules/Newsletter/Newsletter.jsx';
+import { RightsAndTerms } from 'components/Molecules/RightsAndTerms/RightsAndTerms.jsx';
+
+const Footer = () => {
+  return (
+    <div className={css.footer}>
+      <div className={css.footerMain}>
+        <div className={css.footerContent}>
+          <FooterInfo />
+          <FooterLinks />
+          <Newsletter />
+        </div>
+        <FooterIcons />
+      </div>
+      <RightsAndTerms />
+    </div>
+  );
+};
 
 export { Footer };
