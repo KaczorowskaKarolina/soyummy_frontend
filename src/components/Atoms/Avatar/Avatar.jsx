46050587da228179a898d12e9f41386e324a9eca
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2024-03-05 22:40:28 +0100

    Clear the code

--- src/components/Atoms/Avatar/Avatar.jsx
+++ src/components/Atoms/Avatar/Avatar.jsx
@@ -2,32 +2,20 @@ import css from './Avatar.module.css';
 
 import { ReactComponent as PhotoPlaceholder } from '../../../images/icons/avatar-icon.svg';
 
-const Avatar = ({ userName, photoUrl, Header }) => {
-  if (photoUrl && Header) {
-    return (
-      <div className={css.headerPhoto}>
-        <img src={photoUrl} alt={userName} />
-      </div>
-    );
-  } else if (Header) {
-    return (
-      <div className={css.headerPlaceholder}>
-        <PhotoPlaceholder />
-      </div>
-    );
-  } else if (photoUrl) {
-    return (
-      <div className={css.headerPhoto}>
-        <img src={photoUrl} alt={userName} />
-      </div>
-    );
-  } else {
+const Avatar = ({ userName, photoUrl }) => {
+  if (!photoUrl) {
     return (
       <div className={css.headerPlaceholder}>
         <PhotoPlaceholder />
       </div>
     );
   }
+
+  return (
+    <div className={css.headerPhoto}>
+      <img src={photoUrl} alt={userName} />
+    </div>
+  );
 };
 
 export { Avatar };
