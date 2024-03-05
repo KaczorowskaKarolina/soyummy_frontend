5b44b2a9d4a217d50ea3853a051c0badfba14bdd
mattmalicki <mateusz.malicki96@gmail.com>
2024-03-05 17:02:46 +0100

    Adding new selectors.

--- src/redux/categories/selectors.js
+++ src/redux/categories/selectors.js
@@ -1,3 +1,5 @@
 const selectCategories = state => state.categories.items;
+const selectCategoriesError = state => state.categories.error;
+const selectCategoriesLoading = state => state.categories.isLoading;
 
-export { selectCategories };
+export { selectCategories, selectCategoriesError, selectCategoriesLoading };
