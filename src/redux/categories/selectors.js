a97315d4e11d821fc8c36c9845120f533193fad9
mattmalicki <mateusz.malicki96@gmail.com>
2024-03-05 16:41:48 +0100

    Deleted unsued selectors.

--- src/redux/categories/selectors.js
+++ src/redux/categories/selectors.js
@@ -1,4 +1,3 @@
 const selectCategories = state => state.categories.items;
-const selectQuery = state => state.categories.query;
 
-export { selectCategories, selectQuery };
+export { selectCategories };
