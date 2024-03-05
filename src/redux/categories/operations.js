d639089aa03ed5c7874ec4a639411b01483c0e7e
mattmalicki <mateusz.malicki96@gmail.com>
2024-03-05 16:40:27 +0100

    Deleted unused functions.

--- src/redux/categories/operations.js
+++ src/redux/categories/operations.js
@@ -1,21 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const fetchCategories = createAsyncThunk(
-  'categories/fetchFirstCategories',
-  async (_, thunkAPI) => {
-    const state = thunkAPI.getState();
-    const { query } = state.categories;
-    try {
-      const response = await axios.get(`/getAllCategoriesList?query=${query}`);
-      const file = response.data.data.file;
-      return file;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.message);
-    }
-  }
-);
-
 const fetchAllCategories = createAsyncThunk(
   'categories/fetchAll',
   async (_, thunkAPI) => {
@@ -28,15 +13,4 @@ const fetchAllCategories = createAsyncThunk(
   }
 );
 
-const changeQuery = createAsyncThunk(
-  'categories/changeQuery',
-  async (payload, thunkAPI) => {
-    try {
-      return payload;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.message);
-    }
-  }
-);
-
-export { fetchCategories, changeQuery, fetchAllCategories };
+export { fetchAllCategories };
